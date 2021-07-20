91318530323d25f0c063ba2107202c8413782d7f
Rob Morandini <robjmorandini@hotmail.com>
2021-07-19 20:27:23 -0400

    Work on adding character location data to server side, mongoDB connection

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const imageController = require('../controllers/imageController');
+const async = require('async');
+const fs = require('fs');
+const path = require('path');
+
+let currentImageData;
+
+fs.readdir('./imageData/', (err, files) => {
+  if (err) {
+    throw err;
+  }
+  console.log(files);
+}, (err, files) => {
+  if (err) {
+    throw err;
+  } else { 
+    console.log(files);
+    console.log('done reading .json files');
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', imageSrc: 'wheres-waldo-beach.png', characters: ['Waldo', 'Odlaw'] });
 });
 
-router.get('/image:name', imageController.image_detail);
+//router.get('/image:name', imageController.image_detail);
 
 module.exports = router;
